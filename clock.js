5d093228a31d61c833b8fcb697838ca682563d4f
Bunyameen <bunyameen@gmail.com>
2022-12-21 18:10:56 +0100

    Clock: reduce functionality added

--- clock.js
+++ clock.js
@@ -1,18 +1,21 @@
-/**<button id="break-minus-element">-</button>
-    <span id="break-length">5</span>
-    <button id="break-plus-element">+</button>
+let breakLength = 5 * 60;
+let sessionLength = 25 * 60
 
-    <div class="session-length">
-        <h2>Break Session Length</h2>
-        <button id="session-minus-element">-</button>
-        <span id="session-length">25</span>
-        <button id="session-plus-element">+</button>
-    </div> */
 
 
     const breakMinusElement = document.getElementById('break-minus-element')
-    const breakLength = document.getElementById('break-length')
+    const breakElement = document.getElementById('break-element')
     const breakPlusElement = document.getElementById('break-plus-element')
     const sessionMinusElement = document.getElementById('session-minus-element')
-    const sessionLength = document.getElementById('session-length')
+    const sessionElement = document.getElementById('session-element')
     const sessionPlusElement = document.getElementById('session-plus-element')
+
+
+    breakMinusElement.addEventListener('click', () =>{
+
+        breakLength -= 60;
+        breakElement.textContent = breakLength / 60
+
+    })
+
+    
