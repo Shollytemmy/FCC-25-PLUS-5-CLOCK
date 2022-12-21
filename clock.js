b271c10b5ac45f8713c12240fb3d966157af1593
Bunyameen <bunyameen@gmail.com>
2022-12-21 18:46:38 +0100

    Clock: increase functionality added

--- clock.js
+++ clock.js
@@ -1,3 +1,5 @@
+
+isSessionMode = true;
 let breakLength = 5 * 60;
 let sessionLength = 25 * 60
 
@@ -9,6 +11,8 @@ let sessionLength = 25 * 60
     const sessionMinusElement = document.getElementById('session-minus-element')
     const sessionElement = document.getElementById('session-element')
     const sessionPlusElement = document.getElementById('session-plus-element')
+    const timerMinute = document.getElementById('timer-minute')
+    const timerSeconds = document.getElementById('timer-seconds')
 
 
     breakMinusElement.addEventListener('click', () =>{
@@ -24,14 +28,28 @@ let sessionLength = 25 * 60
         breakElement.textContent = breakLength / 60
     })
 
+    // Session
+
     sessionMinusElement.addEventListener('click', () => {
         sessionLength -= 60;
 
         sessionElement.textContent = sessionLength / 60
+
+         if(isSessionMode){
+            timerMinute.textContent = sessionLength / 60
+        }
     })
+
+    
 
 
     sessionPlusElement.addEventListener('click', () => {
         sessionLength += 60;
         sessionElement.textContent = sessionLength / 60
+
+        if(isSessionMode){
+            timerMinute.textContent = sessionLength / 60
+        }
+
+       
     })
