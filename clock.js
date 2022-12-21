ea193a8c7972ff16a1f9ba6b4062eb1969b0f739
Bunyameen <bunyameen@gmail.com>
2022-12-21 17:53:21 +0100

    Fcc Clock: WIP Js  part

---
+++ clock.js
@@ -0,0 +1,18 @@
+/**<button id="break-minus-element">-</button>
+    <span id="break-length">5</span>
+    <button id="break-plus-element">+</button>
+
+    <div class="session-length">
+        <h2>Break Session Length</h2>
+        <button id="session-minus-element">-</button>
+        <span id="session-length">25</span>
+        <button id="session-plus-element">+</button>
+    </div> */
+
+
+    const breakMinusElement = document.getElementById('break-minus-element')
+    const breakLength = document.getElementById('break-length')
+    const breakPlusElement = document.getElementById('break-plus-element')
+    const sessionMinusElement = document.getElementById('session-minus-element')
+    const sessionLength = document.getElementById('session-length')
+    const sessionPlusElement = document.getElementById('session-plus-element')
